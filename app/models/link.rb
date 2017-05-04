1163242c84822cda987ca4b6b4f951bce4a69586
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-05-04 11:46:13 +0100

    changed production env

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,6 +10,6 @@ class Link
 
 end
 
-DataMapper.setup(:default, ENV['https://jons-bookmark-manager.herokuapp.com/'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
